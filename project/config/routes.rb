a35036b3d3424f1180831a5b9a42ced8a517d87a
Matthew Pick <mpick@stanford.edu>
2015-01-27 20:56:20 -0800

    added photo manually to routes.rb and fixed error in scrimage migration

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
 
+  resources :photo
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -61,4 +63,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # get ':controller(/:action(/:id))'
+  # post ':controller(/:action(/:id))'
 end
--- project/db/migrate/20150124213702_create_scrimages.rb
+++ project/db/migrate/20150124213702_create_scrimages.rb
@@ -2,7 +2,7 @@ class CreateScrimages < ActiveRecord::Migration
   def change
     create_table :scrimages do |t|
     	t.datetime :start_time
-    	t.datetime :end_time null: false
+    	t.datetime :end_time, default: false
     	t.integer :original_photo_id
     	t.string :description
     	t.integer :winner_id
