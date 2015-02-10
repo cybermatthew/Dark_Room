4c3c6438a4bf070dd9174a7724b24cbf03eccc7b
Matthew Pick <mpick@stanford.edu>
2015-02-10 15:20:41 -0800

    fixed issue with comments not being added with js

--- project/app/assets/javascripts/hoverIcons.js
+++ project/app/assets/javascripts/hoverIcons.js
@@ -1,15 +1,3 @@
-// $(function(){
-// 	console.log("I'm Here!!!");
-// 	$(".editableImage").hover(
-// 		function(){
-// 			$(this).find(".hoverIcons").show();
-// 			$(this).find(".hoverIcons").css("display", "inline");
-// 		},
-// 		function(){
-// 			$(this).find(".hoverIcons").hide();
-// 	});
-// });
-
 function initHoverIcons(){
 	$(".editableImage").hover(
 		function(){
