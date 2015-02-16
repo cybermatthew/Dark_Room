f14ff25ded91bd6bb4efd702b6f5284a9235cde7
Matthew Pick <mpick@stanford.edu>
2015-02-15 16:18:39 -0800

    browse now shows scrimages instead of photos

--- project/app/controllers/scrimages_controller.rb
+++ project/app/controllers/scrimages_controller.rb
@@ -1,9 +1,14 @@
 class ScrimagesController < ApplicationController
 	def show
-		@scrimage = Scrimage.find(params[:id])
-		@remainingSeconds = (@scrimage.end_time.to_time - DateTime.now.to_time).to_i
+		if !params[:id].nil?
+			@scrimage = Scrimage.find(params[:id])
+			@remainingSeconds = (@scrimage.end_time.to_time - DateTime.now.to_time).to_i
 
-		@original_photo = Photo.where("scrimage_id = ? AND parent_photo_id = ?", @scrimage.id, -1).first
+			@original_photo = Photo.where("scrimage_id = ? AND parent_photo_id = ?", @scrimage.id, -1).first
+		else
+			@photos = Photo.where("parent_photo_id = ?", -1).order("created_at desc")
+			render :action => "show_all"
+		end
 		# @photos = Photo.where("scrimage_id = ? AND parent_photo_id != ?", @scrimage.id, -1)
 	end
 
@@ -42,7 +47,6 @@ class ScrimagesController < ApplicationController
 			originalPhoto.save()
 
 			if originalPhoto.id
-
 				redirect_to :action => "show", :id => scrimage.id
 			else
 				render :action => "new_scrimage"
