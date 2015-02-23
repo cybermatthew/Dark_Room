c10589b0b656079d0c95ec920aaa4a38ee8965b5
Michelle Pan <mxpan@stanford.edu>
2015-02-22 21:40:36 -0800

    forgot to add files to last commit

--- project/app/controllers/home_controller.rb
+++ project/app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
 		@new_photos = Photo.order("id DESC").limit(4)
 		@scrimage = Scrimage.order("RANDOM()").first
 		@originalPhoto = Photo.where("parent_photo_id = ? AND scrimage_id = ?", -1, @scrimage.id).first
-		@childPhotos = Photo.where("parent_photo_id != ? AND scrimage_id = ?", -1, @scrimage.id).limit(5)
+		@childPhotos = Photo.where("parent_photo_id != ? AND scrimage_id = ?", -1, @scrimage.id).order("votes DESC").limit(5)
 	end
 end
